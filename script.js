ac7bd0590de7356b91f299eda8ce107c51bf4c42
BuildTools <unconfigured@null.spigotmc.org>
2022-03-31 22:49:18 +0700

    fix bugs / add responsive

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 window.onload=function(){
+    var header = document.getElementById('header');
+    var mobileMenu = document.getElementById('mobile-menu');
+    var headerHeight = header.clientHeight
+
+    mobileMenu.onclick = function() {
+        var isClose = header.clientHeight === headerHeight;
+        if (isClose) {
+            header.style.height = 'auto';
+        } else {
+            header.style.height = null;
+        }
+    }
+
+    var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+    
+    for (var i = 0; i < menuItems.length; i++) {
+        var menuItem = menuItems[i];
+        
+        menuItem.onclick = function(event) {
+            var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+            if (isParentMenu) {
+                event.preventDefault();
+            } else {
+                header.style.height = null;
+            }
+        }
+    }
+
     const buyBtns = document.querySelectorAll('.js-buy-ticket')
     const modal = document.getElementById('modal')
     const modalContainer = document.querySelector('.js-model-container')
